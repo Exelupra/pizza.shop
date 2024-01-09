d25e57ae726a2d9d08f87aa9994edd0f725dfac5
NicoRiri <nicolate.bernardet@gmail.com>
2024-01-09 17:53:02 +0100

    fix : correction push merde getProduit

--- gateway.pizza-shop/src/app/actions/getProduits.php
+++ gateway.pizza-shop/src/app/actions/getProduits.php
@@ -12,15 +12,11 @@ class getProduits extends AbstractAction
 
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
-        try{
-            $client = new Client();
-            $res = $client->request('GET', "http://api.catalogue.pizza-shop/produits");
-            $res = $res->getBody()->getContents();
-            $response->getBody()->write($res);
-            $response->withStatus(200);
-            return $response;
-        } catch(HttpNotFoundException $e){
-            return $response->withStatus(404);
-        }
+        $client = new Client();
+        $res = $client->request('GET', "http://api.catalogue.pizza-shop/produits");
+        $res = $res->getBody()->getContents();
+        $response->getBody()->write($res);
+        $response->withStatus(200);
+        return $response;
     }
 }
