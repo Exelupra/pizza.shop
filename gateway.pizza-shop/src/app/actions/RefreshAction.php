dadcb46df6c24f02f6edd9c91fd9892144f6df0e
NicoRiri <nicolate.bernardet@gmail.com>
2024-01-09 17:47:19 +0100

    fix : correction push merde refresh

--- gateway.pizza-shop/src/app/actions/RefreshAction.php
+++ gateway.pizza-shop/src/app/actions/RefreshAction.php
@@ -2,11 +2,13 @@
 
 namespace pizzashop\gateway\app\actions;
 
+use Cassandra\Exception\UnauthorizedException;
 use GuzzleHttp\Client;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\Exception\HttpBadRequestException;
 use Slim\Exception\HttpNotFoundException;
+use Slim\Exception\HttpUnauthorizedException;
 
 class RefreshAction extends AbstractAction
 {
@@ -15,20 +17,16 @@ class RefreshAction extends AbstractAction
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
     {
         try {
+            $headers = $request->getHeaders();
 
-
-        $headers = $request->getHeaders();
-
-        $client = new Client();
-        $res = $client->request('POST', "http://api.pizza-auth/api/users/refresh", ['headers' => $headers]);
-        $res = $res->getBody()->getContents();
-        $response->withStatus(200);
-        $response->getBody()->write($res);
-        return $response;
-    } catch(HttpNotFoundException $e){
-        return $response->withStatus(404);
-    } catch(HttpBadRequestException $e){
-        return $response->withStatus(400);
-    }
+            $client = new Client();
+            $res = $client->request('POST', "http://api.pizza-auth/api/users/refresh", ['headers' => $headers]);
+            $res = $res->getBody()->getContents();
+            $response->withStatus(200);
+            $response->getBody()->write($res);
+            return $response;
+        } catch (HttpUnauthorizedException $exception) {
+            return $response->withStatus(40);
+        }
     }
 }
